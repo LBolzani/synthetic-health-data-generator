cb18768d43e9336747761d4ff6ec58610969fafd
luca.bolzani <luca.bolzani@uni.lu>
2022-11-09 12:08:54 +0100

    Add Jupiter folder

--- web-ui/src/main.py
+++ web-ui/src/main.py
@@ -7,31 +7,33 @@ st.title('Uber pickups in NYC')
 DATE_COLUMN = 'date/time'
 #DATA_URL = ('https://s3-us-west-2.amazonaws.com/''streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-DATA_URL='/Users/luca.bolzani/Downloads/uber-raw-data-sep14.csv.gz'
-
 @st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
+def load_data(file_path, nrows):
+    data = pd.read_csv(file_path, nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache)")
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
+uploaded_file = st.file_uploader("Choose a file")
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+if uploaded_file is not None:
+    data_load_state = st.text('Loading data...')
+    data = load_data(uploaded_file, 10000)
+    data_load_state.text("Done! (using st.cache)")
 
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+    if st.checkbox('Show raw data'):
+        st.subheader('Raw data')
+        st.write(data)
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+    st.subheader('Number of pickups by hour')
+    hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+    st.bar_chart(hist_values)
+
+    # Some number in the range 0-23
+    #hour_to_filter = st.slider('hour', 0, 23, 17)
+    #filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+    #st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+    #st.map(filtered_data)
