03f655759c0b19854f011c91cb8b703b8b2834a8
luca.bolzani <luca.bolzani@uni.lu>
2022-11-11 10:29:05 +0100

    Add Output Selection

--- web-ui/src/main.py
+++ web-ui/src/main.py
@@ -24,17 +24,24 @@ def compute_syntData(df_real_data, algorithm, parameters):
     compareModel = ComparisonModels(df_real_data, algos[algorithm], parameters)
     return compareModel.GenerateSynth(parameters)
 
+@st.cache
+def convert_df(data_frame):
+    return data_frame.to_csv().encode('utf-8')
 
 with col1:
     st.subheader("Settings")
     st.file_uploader("Upload data:", key='upload_file')
 
-    algorithm = st.selectbox('Algorithm:',(ctan, copulaGan, gaussianCopula,tvae), key= 'algorithm_gen')
-    parameters = st.selectbox('Parameters:', ('gaussian', 'gamma', 'beta', 'student_t', 'gaussian_kde', 'truncated_gaussian'), key= 'param_gen')
+    st.selectbox('Algorithm:',(ctan, copulaGan, gaussianCopula,tvae), key= 'algorithm_gen')
+    st.selectbox('Parameters:', ('gaussian', 'gamma', 'beta', 'student_t', 'gaussian_kde', 'truncated_gaussian'), key= 'param_gen')
 
-    evaluators_algo = st.radio('Evaluators:',
-                               (evalFN.PCA, evalFN.SVD, evalFN.RANDOM_FOREST,
-                                evalFN.LINEAR_REG, evalFN.CLASSIFICATION, evalFN.REAL_VS_SYNTH),
+    if st.session_state.upload_file is not None:
+        df_real_data = load_data(st.session_state.upload_file)
+        st.selectbox('Output variable:', [col for col in df_real_data.columns], key='output_var')
+
+
+    st.radio('Evaluators:', (evalFN.PCA, evalFN.SVD, evalFN.RANDOM_FOREST,
+                                evalFN.LINEAR_REG, evalFN.REAL_VS_SYNTH),
                                key='algo_eval')
 
     st.markdown("<hr>", unsafe_allow_html=True)
@@ -49,7 +56,7 @@ with col1:
 
 with col2:
     st.subheader("Analysis")
-    tab1, tab2 = st.tabs(["🗃 Data", "📈 Chart"])
+    tab1, tab2 = st.tabs(["🗃 Synth Data", "📈 Chart"])
     if st.session_state.upload_file is not None and st.session_state.gen_synth_data:
         df_real_data = load_data(st.session_state.upload_file)
         df_synth_data, score = compute_syntData(df_real_data, st.session_state.algorithm_gen,
@@ -60,12 +67,19 @@ with col2:
             if score is not None and df_synth_data is not None:
                 st.write("Score: {}".format(score))
                 st.dataframe(df_synth_data)
+                st.download_button(
+                    label = "Download CSV",
+                    data = convert_df(df_synth_data),
+                    file_name='.csv',
+                    mime='text/csv',
+                )
         else:
             st.info("Upload a file in 'Settings' to have your synthetic data!")
 
     with tab2:
         if st.session_state.upload_file is not None and st.session_state.gen_synth_data:
-            evaluator = Evaluator(df_real_data, df_synth_data, df_real_data['diagnosis'])
+            output = st.session_state.output_var
+            evaluator = Evaluator(df_real_data, df_synth_data, df_real_data[output])
             evaluator.preprocess()
             evaluator.do_clustering()
             if st.session_state.algo_eval == evalFN.PCA:
@@ -74,9 +88,12 @@ with col2:
                 st.pyplot(evaluator.plot_svd())
             elif st.session_state.algo_eval == evalFN.RANDOM_FOREST:
                 st.pyplot(evaluator.plot_random_forest())
-            elif st.session_state.algo_eval == evalFN.CLASSIFICATION:
+            elif st.session_state.algo_eval == evalFN.LINEAR_REG:
                 st.pyplot(evaluator.plot_correlation())
+            elif st.session_state.algo_eval == evalFN.REAL_VS_SYNTH:
+                st.pyplot(evaluator.plot_data_2dims())
             else:
-                st.pyplot(evaluator.plot_pca())
+                st.warning("No plot found with algo name {}. Please verify your code."
+                           .format(st.session_state.algo_eval))
         else:
             st.info("Upload a file in 'Settings' to have your synthetic data!")
